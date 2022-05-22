322bcb952df310f72d70d097c7c1c47318d8e260
Anindya Dey <andy.ani1992@gmail.com>
2022-05-22 23:47:49 +0530

    add pointers and arrays

--- pointers.c
+++ pointers.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
+
+  // POINTERS
   int a = 10;
   int *p = &a;
 
@@ -8,8 +10,18 @@ int main() {
   printf("Value in pointer 'p': %p\n", p);
   printf("Value of 'a': %d\n", a);
   printf("Value pointed by pointer 'p': %d\n", *p); // Dereference
-
   printf("Adding ONE to pointer 'p': %p\n", p + 1); // should print an address which is 4 bytes more than 'p'
+
+  // POINTERS AND ARRAYS
+  int A[5] = {2, 3, 4, 5, 6};
+  printf("Address of first element of array A: %p\n", A);
+  printf("Second element in A: %d\n", *(A + 1));
+  for (int i = 0; i < 5; i++) {
+    printf("Address of A[%d]: %p\n", i, &A[i]);
+    printf("Address of A[%d]: %p\n", i, A + i);
+    printf("Value at A[%d]: %d\n", i, A[i]);
+    printf("Value at A[%d]: %d\n", i, *(A + i));
+  }
 
   return 0;
 }
